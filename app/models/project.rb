10e84a7cb3e1296cce8dff4bd4ffb2d5dbdd3cc5
Rick Beyer <rbeyer@patientkeeper.com>
2013-09-18 11:40:59 -0400

    fixed validator to you know, be written corretly.  Thats what I get for hacking!

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,13 +6,12 @@ class Project < ActiveRecord::Base
   
   scope :active, where(:active => true)
   
-  before_validation :verify_scm
+  validate :verify_scm
   
   def verify_scm
   	self.scm_type.downcase!
   	if !self.scm_type.eql?("svn") and !self.scm_type.eql?("git")
-  		errors[:base] << "SCM must be either SVN or Git!"
-  		return false
+  		errors.add(:scm_type," must be either SVN or Git")
   	end
   end
   
