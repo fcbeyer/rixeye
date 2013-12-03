a08b84d5cfdeb0a5dca204ea8bc33c491c50de5f
Rick Beyer <rbeyer@patientkeeper.com>
2013-12-03 10:27:54 -0500

    hopefully fixed sass issues

--- app/models/project.rb
+++ app/models/project.rb
@@ -17,7 +17,8 @@ class Project < ActiveRecord::Base
   
   def parse_xml(log_file,problem,have_new_revision_data)
   	begin
-  		response = system "svn log --verbose --username #{Rixeye::Application.config.rixeye_settings['SVN_User']} --password #{Rixeye::Application.config.rixeye_settings['SVN_Password']} #{self.url_path} -r#{self.next_revision}:HEAD --xml > #{log_file}"
+  		next_revision = self.last_revision.nil? ? self.base_revision : self.last_revision + 1
+  		response = system "svn log --verbose --username #{Rixeye::Application.config.rixeye_settings['SVN_User']} --password #{Rixeye::Application.config.rixeye_settings['SVN_Password']} #{self.url_path} -r#{next_revision}:HEAD --xml > #{log_file}"
   		doc = Nokogiri::XML(File.open(log_file))
   		if !doc.xpath('/log/logentry').empty?
 		  		Commit.parse_xml(doc.xpath('/log/logentry'),self)		  	
