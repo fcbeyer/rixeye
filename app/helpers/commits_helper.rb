5bfe17c62a26b3b2a32ff5c9327f2caf2517c18b
Rick Beyer <rick.beyer@gmail.com>
2014-02-07 15:34:58 -0500

    stripping out all jira and localized naming conventions.  Rewiring it all so its more customizable by display

--- app/helpers/commits_helper.rb
+++ app/helpers/commits_helper.rb
@@ -1,20 +1,16 @@
 module CommitsHelper
 	
-	def create_jira_link(commit)
+	def create_issue_tracker_link(commit)
 		if commit.issue.nil?
 			#no link to create
 		else
 			#we have something to link
-			return raw("<a href=\"#{Rixeye::Application.config.rixeye_settings['Jira_URL']}#{commit.issue}\">#{commit.issue}</a></li>")
+			return raw("<a href=\"#{Rixeye::Application.config.rixeye_settings['Issue_Tracker_Url']}#{commit.issue}\">#{commit.issue}</a></li>")
 		end
 	end
 	
-	def create_fisheye_link(commit,project)
-		return raw("<a href=\"#{Rixeye::Application.config.rixeye_settings['Fisheye_URL']}/#{project.name}?cs=#{commit.revision_number}\">#{commit.revision_number}</a></li>")
-	end
-	
-	def create_collabnet_link(commit,project)
-	  return raw("<a href=\"#{Rixeye::Application.config.rixeye_settings['CollabNet_URL']}&revision=#{commit.revision_number}\">#{commit.revision_number}</a></li>")
+	def create_code_diff_link(commit,project)
+		return raw("<a href=\"#{Rixeye::Application.config.rixeye_settings['Code_Diff_URL']}/#{project.name}?cs=#{commit.revision_number}\">#{commit.revision_number}</a></li>")
 	end
 	
 	def find_paths(commit)
--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,4 @@ Rixeye::Application.initialize!
 
 Rixeye::Application.configure do
 	 config.rixeye_settings = YAML::load(File.open('./config/settings.yml'))["#{ENV['RAILS_ENV']}"]
-	 # config.svn_user = settings['svn_user']
-	 # config.svn_password = settings['svn_password']
-	 # config.git_user = settings['git_user']
-	 # config.git_password = settings['git_password']
-	 # config.jira_url = settings['jira_url']
-	 # config.jira_project_list = settings['jira_project_list']
-	 # config.fisheye_url = settings['fisheye_url']
 end
